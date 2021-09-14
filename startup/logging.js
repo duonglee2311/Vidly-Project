f9534bc3c662341ea52d3b9d8c33adb02134c140
duonglee2311 <duonglee2311@gmail.com>
2021-09-14 17:33:58 +0700

    change startup/logging.js

--- startup/logging.js
+++ startup/logging.js
@@ -3,19 +3,24 @@ const winston = require('winston');
 require('express-async-errors');
 
 module.exports = function() {
-  winston.add(new winston.transports.File( {
-    filename: 'exceptions.log',
-    level: 'error',
-    handleExceptions: true,
-    handleRejections: true
-  }))
-  .add(new winston.transports.File({ 
-    filename: 'logfile.log',
-    level: 'info',
-    format: winston.format.combine(winston.format.timestamp(), winston.format.json())
-  }));
-  // .add(new winston.transports.MongoDB({
-  // db: 'mongodb://localhost:27017/vidly',
-  // options: {useUnifiedTopology: true}
-  // }));
+  winston.exceptions.handle(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ 
+      level: 'error',
+      filename: 'exceptions.log' 
+    }));
+
+    process.on('unhandledRejection', (ex) => {
+      throw ex;
+  });
+  winston
+    .add(new winston.transports.File({ 
+      filename: 'logfile.log',
+      level: 'info',
+      format: winston.format.combine(winston.format.timestamp(), winston.format.json())
+    }));
+    // .add(new winston.transports.MongoDB({
+    // db: 'mongodb://localhost:27017/vidly',
+    // options: {useUnifiedTopology: true}
+    // }));
 }
